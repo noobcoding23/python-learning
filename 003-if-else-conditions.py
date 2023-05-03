87221737a319d115b9e45898efa95a5d88b7f6d9
noobcoding23 <jugalkdas99@gmail.com>
2023-05-03 23:36:38 +0530

    new file names for ordering classes

---
+++ 003-if-else-conditions.py
@@ -0,0 +1,23 @@
+# Conditional Operators:
+# "<", ">", "<=", ">=", "==", "!="
+# a = int(input("Enter your age: "))
+# # print(a>18)
+# # print(a<=18)
+# # print(a==18)
+# # print(a!=18)
+# # print(a<18)
+
+# if (a>=18):
+#     print("You can drive!")
+# else:
+#     print("You cannot drive!")
+
+num = int(input("Enter a number: "))
+if (num < 0):
+    print("The number is negative")
+elif (num == 0):
+    print("The number is zero")
+elif (num == 999):
+    print("the number is special")
+else:
+    print("The number is positive")
