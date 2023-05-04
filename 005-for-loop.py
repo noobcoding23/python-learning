ba9e29a69f4438af8ae13845b54122020a39fdf1
noobcoding23 <jugalkdas99@gmail.com>
2023-05-04 23:38:41 +0530

    for loop added

---
+++ 005-for-loop.py
@@ -0,0 +1,22 @@
+# For loop for strings
+name = "Jugalkdas"
+for i in name:
+    print(i, end=", ")
+print()
+colors = ["Red", "Green", "Blue", "Yellow", "Orange"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+# For loop for strings
+
+# For loop for numbers
+for i in range(5): # Prints 0 to 4
+    print(i, end=", ")
+print()
+for y in range(3, 11): # Prints 3 to 10
+    print(y, end=", ")
+print()
+for z in range(4, 20, 2): # Third parameter is for steps
+    print(z, end=", ")
+# For loop for numbers
