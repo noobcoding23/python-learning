22cdfdce21cb3bd5a15571132042153e97341263
noobcoding23 <jugalkdas99@gmail.com>
2023-05-05 20:43:09 +0530

    while loop added

---
+++ 006-while-loop.py
@@ -0,0 +1,15 @@
+i = 0
+while (i <= 10):
+    print(i, end=", ")
+    i += 1 # Or i = i + 1
+print()
+count = 5
+while (count > 0):
+    print(count, end=", ")
+    count -= 1 # Or count = Count -1
+    # Revers loops
+else: # When the while loop condition became false then the the else condition runs
+    print()
+    print("This loop ends here")
+print()
+print()
