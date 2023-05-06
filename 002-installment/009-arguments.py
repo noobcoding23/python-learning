1704add0b1411201debe819ee6b85732fb78ca42
noobcoding23 <jugalkdas99@gmail.com>
2023-05-06 19:48:51 +0530

    argument of functions added

---
+++ 002-installment/009-arguments.py
@@ -0,0 +1,5 @@
+def average(a=9, b=1):
+    print("The average is", (a+b)/2)
+average() # Returns 5 according to the valuew of a and b inside the functions
+average(5, 15) # Returns 10 cause the value assigned her will replace the mentioned above values of  a, b
+average(1) # Here a=1 and the value of b is the default value which is mentioned above
