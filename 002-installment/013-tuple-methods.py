7dda4b4e66e0d6b2b9f2c1f199badc414fc0fa90
noobcoding23 <jugalkdas99@gmail.com>
2023-05-07 21:27:22 +0530

    more from index

--- 002-installment/013-tuple-methods.py
+++ 002-installment/013-tuple-methods.py
@@ -1,25 +1,3 @@
-tp1 = (1, 2, 3, 4, 5)
-tp2 = (6, 7, 8, 9, 10)
-tpMerge = tp1 + tp2
-print(tpMerge) # Merging two tuples in a new tuple
-testTp = (1, 2, 2, 3, 4, 2, 70)
-print(testTp.count(2), "<-- Number of time 2 is repeated in the tuple")
-print(testTp.index(2), "<-- Returns the index of 2 in the first occurence")
-
-# <--CodeWithHarry Example-->
-#
-# 
-# countries = ("Spain", "Italy", "India", "England", "Germany")
-# temp = list(countries)
-# temp.append("Russia")       #add item 
-# temp.pop(3)                 #remove item
-# temp[2] = "Finland"         #change item
-# countries = tuple(temp)
-# print(countries)
-# 
-# 
-# <--CodeWithHarry Example-->
-
 # <--My example-->
 # 
 # 
@@ -38,3 +16,26 @@ print(nameOfPeople)
 # 
 # 
 # <--My example-->
+
+# <--Internet Example-->
+#
+# 
+# countries = ("Spain", "Italy", "India", "England", "Germany")
+# temp = list(countries)
+# temp.append("Russia")       #add item 
+# temp.pop(3)                 #remove item
+# temp[2] = "Finland"         #change item
+# countries = tuple(temp)
+# print(countries)
+# 
+# 
+# <--Internet Example-->
+
+tp1 = (1, 2, 3, 4, 5)
+tp2 = (6, 7, 8, 9, 10)
+tpMerge = tp1 + tp2
+print(tpMerge) # Merging two tuples in a new tuple
+testTp = (1, 2, 2, 3, 4, 2, 70)
+print(testTp.count(2), "<-- Number of time 2 is repeated in the tuple")
+print(testTp.index(2), "<-- Returns the index of 2 in the first occurence")
+print(testTp.index(2, 3, 6), "<-- Returns the index of 2 in between the start and end index")# index(ELEMENT, START, END) <-Syntax
