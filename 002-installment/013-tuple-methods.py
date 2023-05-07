c5110d8a4bfbcf1f6188661d372b963481ea684b
noobcoding23 <jugalkdas99@gmail.com>
2023-05-07 20:42:16 +0530

    tuple method added

---
+++ 002-installment/013-tuple-methods.py
@@ -0,0 +1,6 @@
+tp1 = (1, 2, 3, 4, 5)
+tp2 = (6, 7, 8, 9, 10)
+tpMerge = tp1 + tp2
+print(tpMerge) # Merging two tuples in a new tuple
+testTp = (1, 2, 2, 3, 4, 2, 70)
+print(testTp.count(2), "<-- Number of time 2 is repeated in the tuple")
