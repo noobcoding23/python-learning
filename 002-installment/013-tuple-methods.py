1d082e2a4b2368c4865a975fa619122b27b3a571
noobcoding23 <jugalkdas99@gmail.com>
2023-05-07 21:19:02 +0530

    changing tuple with list method

--- 002-installment/013-tuple-methods.py
+++ 002-installment/013-tuple-methods.py
@@ -4,3 +4,37 @@ tpMerge = tp1 + tp2
 print(tpMerge) # Merging two tuples in a new tuple
 testTp = (1, 2, 2, 3, 4, 2, 70)
 print(testTp.count(2), "<-- Number of time 2 is repeated in the tuple")
+print(testTp.index(2), "<-- Returns the index of 2 in the first occurence")
+
+# <--CodeWithHarry Example-->
+#
+# 
+# countries = ("Spain", "Italy", "India", "England", "Germany")
+# temp = list(countries)
+# temp.append("Russia")       #add item 
+# temp.pop(3)                 #remove item
+# temp[2] = "Finland"         #change item
+# countries = tuple(temp)
+# print(countries)
+# 
+# 
+# <--CodeWithHarry Example-->
+
+# <--My example-->
+# 
+# 
+nameOfPeople = ("Rohan", "Ravi", "Jugal", "Nayan")
+print(nameOfPeople)
+tempList = list(nameOfPeople) # Converting to list using list method
+# print(tempList)
+tempList.append("Vijay") # Adding a data
+# print(tempList)
+tempList.pop(3) # Removing a data using pop method with index number 3
+# print(tempList)
+tempList[1] = "Raju"
+# print(tempList)
+nameOfPeople = tuple(tempList) # Converting again to tuple using tuple method
+print(nameOfPeople)
+# 
+# 
+# <--My example-->
