65e0e21e4288d27e8c75b2e409d9a1c9b568b656
noobcoding23 <jugalkdas99@gmail.com>
2023-05-07 19:10:30 +0530

    appended list added as a new file

---
+++ 002-installment/011-list-methods.py
@@ -0,0 +1,9 @@
+# ----------List Methods in python------------
+
+l = [1, 2, 3, 4, 5, 6]
+print(l)
+l.append(7) # This method just adds a new data to a list
+print(l)
+print("^^^^^^")
+print("||||||")
+print("Appended list")
