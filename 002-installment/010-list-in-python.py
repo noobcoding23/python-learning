f521340e451cb99c9056487679eeb4d525cd17c5
noobcoding23 <jugalkdas99@gmail.com>
2023-05-07 14:40:21 +0530

    new datatypes were added

--- 002-installment/010-list-in-python.py
+++ 002-installment/010-list-in-python.py
@@ -1,5 +1,9 @@
-marks = [46, 38, 44]
+marks = [46, 38, 44, "John Doe", True] # Can be added different types of datatype
 print()
 print(marks)
 print(type(marks)) # Printing the datatype of marks
 print(marks[0]) # Using index to print only one data inside the list
+print(marks[1])
+print(marks[2])
+print(marks[3])
+print(marks[4])
