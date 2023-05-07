6dde03abfaf95b0ef735533370de5401594155c9
noobcoding23 <jugalkdas99@gmail.com>
2023-05-07 19:23:02 +0530

    some basic methods added

--- 002-installment/011-list-methods.py
+++ 002-installment/011-list-methods.py
@@ -1,9 +1,13 @@
 # ----------List Methods in python------------
 
-l = [1, 2, 3, 4, 5, 6]
+l = [1, 77, 12, 99, 17, 2]
 print(l)
+l.reverse()
+print(l, "<- The list in reverse order")
 l.append(7) # This method just adds a new data to a list
-print(l)
-print("^^^^^^")
-print("||||||")
-print("Appended list")
+print(l, "<- Appended list")
+l.sort()
+print(l, "<- Sorted list low->high")
+l.sort(reverse=True)
+print(l, "<- Sorted list reverse order")
+
