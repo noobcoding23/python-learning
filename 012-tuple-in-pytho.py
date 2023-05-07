618ec5a8e7c7f9169dc82e8257baa51ec27958e1
noobcoding23 <jugalkdas99@gmail.com>
2023-05-07 19:56:55 +0530

    tuple behaviors

---
+++ 012-tuple-in-pytho.py
@@ -0,0 +1,6 @@
+tup = (1, 5, 7)
+print(type(tup), tup)
+tup = (1)
+print(type(tup), tup, "<- this becomes an integer why?")
+tup = (1,)
+print(type(tup), tup, "<- this becomes a tuple") # To tell python that you are creating a one data tuple you have to use one comma after the data
