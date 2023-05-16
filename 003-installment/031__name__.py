1a5d7d2b6d53a24d8446d6f8351e64a0fa77eee1
noobcoding23 <jugalkdas99@gmail.com>
2023-05-16 20:29:58 +0530

    __name__ is finished

---
+++ 003-installment/031__name__.py
@@ -0,0 +1,3 @@
+import jk
+
+jk.welcome()
--- 003-installment/jk.py
+++ 003-installment/jk.py
@@ -2,3 +2,6 @@ def welcome():
     print("Hey there! This is custom module imported here!")
 
 jkp = "Hey welcome here!"
+print(__name__)
+if __name__ == "__main__":
+    welcome()
