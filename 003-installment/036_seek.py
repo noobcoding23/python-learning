92914eb50c7d5c29daeada0b765d1bc068387912
noobcoding23 <jugalkdas99@gmail.com>
2023-05-17 21:39:21 +0530

    seek added

---
+++ 003-installment/036_seek.py
@@ -0,0 +1,7 @@
+with open('003-installment/newfile.txt', 'r') as f:
+    print(type(f))
+    # Move to the 10th byte in the file
+    f.seek(10)
+# Read the next 5 bytes
+data = f.read(5)
+print(data)
