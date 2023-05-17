7f20c6e26c8e7ce32eaf78c4ab2daab876fc04b4
noobcoding23 <jugalkdas99@gmail.com>
2023-05-17 22:05:54 +0530

    seek finished

--- 003-installment/036_seek.py
+++ 003-installment/036_seek.py
@@ -1,7 +1,15 @@
-with open('003-installment/newfile.txt', 'r') as f:
+with open('003-installment/newtext.txt', 'r') as f:
     print(type(f))
     # Move to the 10th byte in the file
     f.seek(10)
-# Read the next 5 bytes
-data = f.read(5)
-print(data)
+    # Read the next 5 bytes
+    print(f.tell()) # Tells where the seek() is started
+    data = f.read(5)
+    print(data)
+
+with open('003-installment/truncate.txt', 'w') as f:
+    f.write('Hello World!')
+    f.truncate(5) # Limits the number of characters can be stored in a file. Here only 'Hello' will be wrote in the truncate.txt file
+
+with open('003-installment/truncate.txt', 'r') as f:
+    print(f.read())
